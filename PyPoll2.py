2b374dcd2a034bf102b0ef5ac5e5c23b1654674f
Takemi <tsoshiro@gmail.com>
2022-05-24 15:05:44 -0700

    re-doing PyPoll as PyPoll2 to attempt higher grade.

---
+++ PyPoll2.py
@@ -0,0 +1,123 @@
+# ADD DEPENDENCIES
+import csv
+import os
+
+
+# Assign a variable for the file to load and the path.
+#file_to_load = 'Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
+
+# Winning Candidate and Winning Count Tracker
+# MODULE 3.5.5
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
+# Open the election results and read the file.
+# election_data = open(file_to_load, 'r')
+with open(file_to_load) as election_data:
+
+    # To do: perform analysis.
+    # print(election_data)
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    # print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        # print(row)
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+
+    # To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+        # If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+
+        # And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+    #  To do: print out the winning candidate, vote count and percentage to
+    #   terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+
+    # 4. Print the candidate name and percentage of votes.
+    # print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
+# Print the candidate vote dictionary.
+# print(candidate_votes)            
+
+        # Add the candidate name to the candidate list.
+        # candidate_options.append(candidate_name)
+
+# Print the candidate list.
+# print(candidate_options)
+
+# 3. Print the total votes.
+# print(total_votes)   
+
+# Using the open() function with the "w" mode we will write data to the file.
+# pen(file_to_save, "w")
+
+# Close the file.
+# election_data.close()
---
+++ Python_practice.py
@@ -0,0 +1,11 @@
+print ("Hello world")
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+for county in counties:
+    print(county)
+    
